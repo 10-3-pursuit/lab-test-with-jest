3b8309be60f15cb613027abe9bc9c0308d5c7175
juligarc91 <julissagarcia@pursuit.org>
2023-10-07 13:10:53 -0400

    Added pseudocode comments to fx getTicketByName

--- src/tickets.js
+++ src/tickets.js
@@ -4,12 +4,12 @@
  * @param {String} name The name of a ticket
  * @returns The ticket object with a matching ID or `null` if no object is found.
  */
-function getTicketByName(tickets, name) {
-  const result = tickets.find((ticket) => {
-    return ticket.name.toLowerCase() === name.toLowerCase();
+function getTicketByName(tickets, name) { // fx is defined with 2 parameters
+  const result = tickets.find((ticket) => { // .find() method returns first element in tickets array that satisfies condition in next line (like using {break}; in a loop with an if statement)
+    return ticket.name.toLowerCase() === name.toLowerCase(); // .find() tests each ticket in tickets array by checking if name property of the ticket (converted to lowercase) matches the provided name argument (also converted to lowercase). .toLowerCase() makes sure search is case insensitive
   });
 
-  return result || null;
+  return result || null; // returns null if no ticket that matches criteria is found (when callback returns undefined which is still truthy so || can work and return null), or the result if callback fx returns an element from array
 }
 
 /**
