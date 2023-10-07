698eb641a157ad6f7d00b12dada96a2e0ca0422c
juligarc91 <julissagarcia@pursuit.org>
2023-10-07 14:01:08 -0400

    Cleaned up comments

--- src/concessions.js
+++ src/concessions.js
@@ -1,30 +1,30 @@
 /**
  * Returns a concession object by ID.
- * @param {Object[]} concessions An array of concession objects
- * @param {String} id A unique ID for a specific concession
+ * @param {Object[]} concessions An array of concession objects.
+ * @param {String} id A unique ID for a specific concession.
  * @returns A concession object with a matching ID or `null` if no object is found.
  */
 function getConcessionByID(concessions, id) { // fx is defined with 2 parameters
    // .find is invoked on concessions array to find and return first element (concessions object) in concessions array that matches id input
    // if it doesn't it it'll return null
   return concessions.find((concession) => concession.id === id) || null;
-}
+};
 
 /**
  * Calculates a total based on the given concession IDs.
- * @param {Object[]} concessions An array of concession objects
- * @param {String[]} ids An array of unique IDs
+ * @param {Object[]} concessions An array of concession objects.
+ * @param {String[]} ids An array of unique IDs.
  * @returns The total value of all concessions from the `ids` array.
  */
 function calculateTotalFromIDs(concessions, ids) { // fx is defined with 2 parameters
   let total = 0; // initialized total with value of 0 (to accumualte total cost after iteration)
   for (let id of ids) { // iterates over each id in ids array
     // stores value of previous fx in concessions variable to use as as a callback function
-    const concession = getConcessionByID(concessions, id); // value of concession = fx getConcessionByID that returns concession object with a matching ID or `null` if no object is found.
+    const concession = getConcessionByID(concessions, id); // value of concession = fx getConcessionByID that returns concession object with a matching ID or `null` if no object is found
     if (concession) { 
       // if callback doesn't evaluate to null (concession object with matching ID exists) then it adds value in property priceInCents to total
       total += concession.priceInCents;
     }
   }
   return total; // returns sum of the total values (prices) of concession object with ids in ids array 
-}
+};
--- src/tickets.js
+++ src/tickets.js
@@ -1,7 +1,7 @@
 /**
  * Returns a ticket object when given the name.
- * @param {Object[]} tickets An array of ticket objects
- * @param {String} name The name of a ticket
+ * @param {Object[]} tickets An array of ticket objects.
+ * @param {String} name The name of a ticket.
  * @returns The ticket object with a matching ID or `null` if no object is found.
  */
 function getTicketByName(tickets, name) { // fx is defined with 2 parameters
@@ -10,21 +10,22 @@ function getTicketByName(tickets, name) { // fx is defined with 2 parameters
   });
 
   return result || null; // returns null if no ticket that matches criteria is found (when callback returns undefined which is still truthy so || can work and return null), or the result if callback fx returns an element from array
-}
+};
 
 /**
  * Calculates a total based on the given ticket names.
- * @param {Object[]} tickets An array of ticket objects
- * @param {String[]} names An array of ticket names
+ * @param {Object[]} tickets An array of ticket objects.
+ * @param {String[]} names An array of ticket names.
  * @returns The total value of all tickets from the `names` array.
  */
 function calculateTotalFromTicketNames(tickets, names) { // fx is defined with 2 parameters
-  let total = 0; // Initializing variable total with value 0. Stores cumulative price of the tickets whose names are provided in the names array.
+  let total = 0; // Initializing variable total with value 0. Stores cumulative price of the tickets whose names are provided in the names array
   for (let name of names) { // Iterates through each name in names array
-    const ticket = getTicketByName(tickets, name); // stores value of previous fx in tickets variable to use as as a callback function
+     // stores value of previous fx in tickets variable to use as as a callback function
+    const ticket = getTicketByName(tickets, name); // value of ticket = fx getTicketByName that returns ticket object with a matching ID or `null` if no object is found
     if (ticket) {
       total += ticket.priceInCents; // if callback doesn't evaluate to null  (ticket with name exists) then it adds value in property priceInCents to total
     }
   }
   return total; // returns sum of the total values (prices) of tickets with names in names array 
-}
+};
