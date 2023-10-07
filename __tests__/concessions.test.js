ee4c87d0340590751c39c9fab89aaeee3d41832b
juligarc91 <julissagarcia@pursuit.org>
2023-10-07 18:34:39 -0400

    Created 2nd test case for fx getConcessionByID

--- __tests__/concessions.test.js
+++ __tests__/concessions.test.js
@@ -3,6 +3,7 @@ const {
     getConcessionByID,
     calculateTotalFromIDs
   } = require('../src/concessions');
+const importedConcessionsData = require ('../data/concessions.js'); // imported concessions data so test can understand structure of input
 
 // --- T E S T S ---
 
@@ -13,5 +14,9 @@ describe(getConcessionByID, () => { // test suite
   
   test("returns null if no object is found", () => {
     // test code
+    const inputArrObj = importedConcessionsData; // array of objects input
+    const inputID = "q1w2e3r4r"; // id string that doesn't exist in array of objects input
+    const actual = getTicketByName(inputArrObj, inputID);
+    expect(actual).toBeNull(); // .toBeNull bc output is expected to be null in this case
   });
 });
