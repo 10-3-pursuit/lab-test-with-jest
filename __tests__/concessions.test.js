d73a25d8e18c35a05b50195444e02a2f6550afe5
juligarc91 <julissagarcia@pursuit.org>
2023-10-07 16:35:20 -0400

    Created test structure to test fx calculateTotalFromTicketNames

--- __tests__/concessions.test.js
+++ __tests__/concessions.test.js
@@ -5,12 +5,22 @@ const {
   } = require('../src/concessions');
 
 // Creating test structure
-describe("getTicketByName", () => {
+describe("getTicketByName", () => { // test suite
   test("returns The ticket object with a matching ID", () => {
     // test code
   });
 
   test("returns `null` if no object is found", () => {
+    // test code
+  });
+});
+
+describe("calculateTotalFromTicketNames", () => { // test suite
+  test("returns calculated total based on the given ticket names.", () => {
+    // test code
+  });
+
+  test("returns numerical value 0 if the ticket name doesn't exist in given array", () => {
     // test code
   });
 });
