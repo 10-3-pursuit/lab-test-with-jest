65a0404739f8dda090e5c92235ae46a75d56d553
juligarc91 <julissagarcia@pursuit.org>
2023-10-07 18:42:26 -0400

    Created test suite for fx calculateTotalFromIDs

--- __tests__/concessions.test.js
+++ __tests__/concessions.test.js
@@ -10,6 +10,11 @@ const importedConcessionsData = require ('../data/concessions.js'); // imported 
 describe(getConcessionByID, () => { // test suite
   test("returns a concession object by ID.", () => {
     // test code
+    const inputArrObj = importedConcessionsData; // array of objects input
+    const inputID = "g9sZdG1hI"; // id string that exists in array of objects input
+    const actual = getTicketByName(inputArrObj, inputID);
+    const expected = { id: "g9sZdG1hI", name: "Small Popcorn", priceInCents: 929 } // output is object with this structure in this case
+    expect(actual).toEqual(expected); // .toEqual for reference types since output expected is an object
   });
   
   test("returns null if no object is found", () => {
@@ -18,5 +23,15 @@ describe(getConcessionByID, () => { // test suite
     const inputID = "q1w2e3r4r"; // id string that doesn't exist in array of objects input
     const actual = getTicketByName(inputArrObj, inputID);
     expect(actual).toBeNull(); // .toBeNull bc output is expected to be null in this case
+  });
+});
+
+describe(calculateTotalFromIDs, () => { // test suite
+  test("returns the total value of all concessions from the `ids` array", () => {
+    // test code
+  });
+
+  test("returns numerical value 0 if the ticket id doesn't exist in given array", () => {
+    // test code
   });
 });
