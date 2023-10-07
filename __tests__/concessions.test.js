fed854303638068d059ae318490063cb98aaf5ae
AnitaOwen <anitaowen@pursuit.org>
2023-10-07 16:22:42 -0400

    added test to get 100 coverage

--- __tests__/concessions.test.js
+++ __tests__/concessions.test.js
@@ -22,6 +22,15 @@ describe("Concessions", ()=>{
         })
     })
 
+    describe("getConcessionByID", ()=>{
+        it("should return null if concession array is empty", ()=>{
+            const input1 = []
+            const input2 = "0Qs9Yp2NL"
+            const actual = getConcessionByID(input1, input2)
+            const expected = null
+            expect(actual).toEqual(expected)
+        })
+    })
 
     describe("calculateTotalFromIDs", ()=>{
         it("should return the total value of all concessions from the array of given ids", ()=>{
@@ -49,6 +58,16 @@ describe("Concessions", ()=>{
             const input2 = ["KzWBehRAD"]
             const actual = calculateTotalFromIDs(input1,input2)
             const expected = 699
+            expect(actual).toEqual(expected)
+        })
+    })
+
+    describe("calculateTotalFromIDs", ()=>{
+        it("should return the total even when given a non-matching id in an array", ()=>{
+            const input1 = concessions
+            const input2 = ["KzWBehaeD", "rNVCeVsri", "NEHAsbTYk", "WAHAsb0Yk"]
+            const actual = calculateTotalFromIDs(input1,input2)
+            const expected = 1318
             expect(actual).toEqual(expected)
         })
     })
