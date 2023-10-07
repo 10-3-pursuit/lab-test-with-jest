0976842bff6dc18e535dfa7e6506976e7cbb7694
juligarc91 <julissagarcia@pursuit.org>
2023-10-07 17:07:29 -0400

    Fixed syntax error in tickets test file line 21

--- __tests__/tickets.test.js
+++ __tests__/tickets.test.js
@@ -8,13 +8,17 @@ describe(getTicketByName, () => { // test suite
   test("returns The ticket object with a matching ID", () => {
     // test code
     const inputArrObj = [
-      { id: "1a2b3c", name: "Adult Matinee", priceInCents: 949 },
-      { id: "4d5e6f", name: "Child Matinee", priceInCents: 749 },
+      { id: "6ha0u__54", name: "Adult Matinee", priceInCents: 949 },
+      { id: "dlb8ka9N_", name: "Senior Matinee", priceInCents: 839 },
+      { id: "FSxeEmcoJ", name: "Child Matinee", priceInCents: 669 },
+      { id: "uD_PoicNN", name: "Adult Regular", priceInCents: 1369 },
+      { id: "TQh2t6HWf", name: "Senior Regular", priceInCents: 1219 },
+      { id: "B8v5R2nVQ", name: "Child Regular", priceInCents: 1069 },
     ];
     const inputTicketName = "Adult Matinee";
     const actual = getTicketByName(inputArrObj, inputTicketName);
     const expected = { id: "6ha0u__54", name: "Adult Matinee", priceInCents: 949 };
-    expected(actual).toEqual(expected); // .toEqual instead of .toBe bc expects reference type
+    expect(actual).toEqual(expected); // .toEqual instead of .toBe bc expects reference type
   });
 
   test("returns `null` if no object is found", () => {
