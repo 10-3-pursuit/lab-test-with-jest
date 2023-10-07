9dd1c7afffdc96a77b04051b4be52d3336a7ab05
juligarc91 <julissagarcia@pursuit.org>
2023-10-07 16:39:43 -0400

    Wrote test for tickets.js in wrong test file made corrections

--- __tests__/concessions.test.js
+++ __tests__/concessions.test.js
@@ -3,24 +3,3 @@ const {
     getConcessionByID,
     calculateTotalFromIDs
   } = require('../src/concessions');
-
-// Creating test structure
-describe("getTicketByName", () => { // test suite
-  test("returns The ticket object with a matching ID", () => {
-    // test code
-  });
-
-  test("returns `null` if no object is found", () => {
-    // test code
-  });
-});
-
-describe("calculateTotalFromTicketNames", () => { // test suite
-  test("returns calculated total based on the given ticket names.", () => {
-    // test code
-  });
-
-  test("returns numerical value 0 if the ticket name doesn't exist in given array", () => {
-    // test code
-  });
-});
--- __tests__/tickets.test.js
+++ __tests__/tickets.test.js
@@ -2,3 +2,24 @@ const {
   getTicketByName,
   calculateTotalFromTicketNames
 } = require('../src/tickets');
+
+// Creating test structure
+describe(getTicketByName, () => { // test suite
+  test("returns The ticket object with a matching ID", () => {
+    // test code
+  });
+
+  test("returns `null` if no object is found", () => {
+    // test code
+  });
+});
+
+describe(calculateTotalFromTicketNames, () => { // test suite
+  test("returns calculated total based on the given ticket names.", () => {
+    // test code
+  });
+
+  test("returns numerical value 0 if the ticket name doesn't exist in given array", () => {
+    // test code
+  });
+});
