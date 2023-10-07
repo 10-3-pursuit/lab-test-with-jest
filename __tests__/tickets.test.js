b48947cb0e1df9169b3857760ddbde7e528b6a65
juligarc91 <julissagarcia@pursuit.org>
2023-10-07 17:22:26 -0400

    Completed test suite for fx getTicketByName

--- __tests__/tickets.test.js
+++ __tests__/tickets.test.js
@@ -1,28 +1,27 @@
 const {
   getTicketByName,
   calculateTotalFromTicketNames
-} = require('../src/tickets');
+} = require('../src/tickets'); // imported functions from tickets.js to be tested
+
+const importedTicketData = require ('../data/tickets.js'); // imported ticket data so test can understand structure of input
 
 // Creating test structure
 describe(getTicketByName, () => { // test suite
-  test("returns The ticket object with a matching ID", () => {
+  test("returns The ticket object with a matching name", () => {
     // test code
-    const inputArrObj = [
-      { id: "6ha0u__54", name: "Adult Matinee", priceInCents: 949 },
-      { id: "dlb8ka9N_", name: "Senior Matinee", priceInCents: 839 },
-      { id: "FSxeEmcoJ", name: "Child Matinee", priceInCents: 669 },
-      { id: "uD_PoicNN", name: "Adult Regular", priceInCents: 1369 },
-      { id: "TQh2t6HWf", name: "Senior Regular", priceInCents: 1219 },
-      { id: "B8v5R2nVQ", name: "Child Regular", priceInCents: 1069 },
-    ];
+    const inputArrObj = importedTicketData;
     const inputTicketName = "Adult Matinee";
     const actual = getTicketByName(inputArrObj, inputTicketName);
     const expected = { id: "6ha0u__54", name: "Adult Matinee", priceInCents: 949 };
     expect(actual).toEqual(expected); // .toEqual instead of .toBe bc expects reference type
   });
 
-  test("returns `null` if no object is found", () => {
+  test("returns `null` if no object with matching ticket name is found", () => {
     // test code
+    const inputArrObj = importedTicketData;
+    const inputTicketName = "Free";
+    const actual = getTicketByName(inputArrObj, inputTicketName);
+    expect(actual).toBeNull(); // .toEqual instead of .toBe bc expects reference type
   });
 });
 
