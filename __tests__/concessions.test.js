ca27da9b1964043462a947a07511c092d7673e98
juligarc91 <julissagarcia@pursuit.org>
2023-10-07 20:36:55 -0400

    Building test cases for fx calculateTotalFromIDs

--- __tests__/concessions.test.js
+++ __tests__/concessions.test.js
@@ -11,8 +11,8 @@ describe(getConcessionByID, () => { // test suite
   test("returns a concession object by ID.", () => {
     // test code
     const inputArrObj = importedConcessionsData; // array of objects input
-    const inputID = "g9sZdG1hI"; // id string that exists in array of objects input
-    const actual = getTicketByName(inputArrObj, inputID);
+    const inputIDStr = "g9sZdG1hI"; // id string that exists in array of objects input
+    const actual = getTicketByName(inputArrObj, inputIDStr);
     const expected = { id: "g9sZdG1hI", name: "Small Popcorn", priceInCents: 929 } // output is object with this structure in this case
     expect(actual).toEqual(expected); // .toEqual for reference types since output expected is an object
   });
@@ -20,18 +20,26 @@ describe(getConcessionByID, () => { // test suite
   test("returns null if no object is found", () => {
     // test code
     const inputArrObj = importedConcessionsData; // array of objects input
-    const inputID = "q1w2e3r4r"; // id string that doesn't exist in array of objects input
-    const actual = getTicketByName(inputArrObj, inputID);
+    const inputIDStr = "q1w2e3r4r"; // id string that doesn't exist in array of objects input
+    const actual = getTicketByName(inputArrObj, inputIDStr);
     expect(actual).toBeNull(); // .toBeNull bc output is expected to be null in this case
   });
 });
 
 describe(calculateTotalFromIDs, () => { // test suite
-  test("returns the total value of all concessions from the `ids` array", () => {
+  test("returns the total value of all valid concessions from the `ids` array", () => {
     // test code
+    const inputArrObj = importedConcessionsData; // array of objects input
+    const inputIDArr = importedConcessionsData.map(item => item.id)
+    const actual = getTicketByName(inputArrObj, inputIDArr);
+    expect(actual).toBe(expected); // .toBe for primitive data types
   });
 
   test("returns numerical value 0 if the ticket id doesn't exist in given array", () => {
     // test code
+    const inputArrObj = importedConcessionsData; // array of objects input
+    const inputIDArr = importedConcessionsData.map(item => item.id)
+    const actual = getTicketByName(inputArrObj, inputIDStrArr);
+    expect(actual).toBe(expected); // .toBe for primitive data types
   });
 });
